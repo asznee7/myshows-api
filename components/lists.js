b8d3dfa4633e7ed0c9ba4fdc2dcc019f4c551762
dotaxe <hunter-lin@yandex.ru>
2018-09-17 01:01:23 +0300

    Add lists methods

--- components/lists.js
+++ components/lists.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const MyShows = require('../index.js')
 const EList = require('../resources/EList')
 
@@ -28,6 +30,60 @@ MyShows.prototype.listsShows = async function(list = EList.FAVORITES) {
 }
 
 /**
+ * Adds a show to favorites list, returns true if adding was successful
+ * @param {int} id - Show id
+ */
+MyShows.prototype.listsAddShow = async function(id) {
+  try {
+    const response = await this.axios.post('', {
+      ...this.defaultParams,
+      method: 'lists.AddShow',
+      params: {
+        id,
+        list: EList.FAVORITES,
+      },
+    })
+
+    const { result } = response.data
+
+    if (result) {
+      return { id, list, result }
+    } else {
+      return { error: response.data.error }
+    }
+  } catch (error) {
+    return { error }
+  }
+}
+
+/**
+ * Removes a show from favorites list, returns true if removing was successful
+ * @param {int} id - Episode id
+ */
+MyShows.prototype.listsRemoveShow = async function(id) {
+  try {
+    const response = await this.axios.post('', {
+      ...this.defaultParams,
+      method: 'lists.RemoveShow',
+      params: {
+        id,
+        list: EList.FAVORITES,
+      },
+    })
+
+    const { result } = response.data
+
+    if (result) {
+      return { id, list, result }
+    } else {
+      return { error: response.data.error }
+    }
+  } catch (error) {
+    return { error }
+  }
+}
+
+/**
  * Returns a list of episodes
  * @param {string} list - A list name (favorites, ignored, unwatched, next)
  */
@@ -45,6 +101,62 @@ MyShows.prototype.listsEpisodes = async function(list = EList.FAVORITES) {
 
     if (result) {
       return { list, result }
+    } else {
+      return { error: response.data.error }
+    }
+  } catch (error) {
+    return { error }
+  }
+}
+
+/**
+ * Adds an episode to a list, returns true if adding was successful
+ * @param {int} id - Episode id
+ * @param {string} list - A list name (favorites, ignored)
+ */
+MyShows.prototype.listsAddEpisode = async function(id, list) {
+  try {
+    const response = await this.axios.post('', {
+      ...this.defaultParams,
+      method: 'lists.AddEpisode',
+      params: {
+        id,
+        list,
+      },
+    })
+
+    const { result } = response.data
+
+    if (result) {
+      return { id, list, result }
+    } else {
+      return { error: response.data.error }
+    }
+  } catch (error) {
+    return { error }
+  }
+}
+
+/**
+ * Removes an episode from a list, returns true if removing was successful
+ * @param {int} id - Episode id
+ * @param {string} list - A list name (favorites, ignored)
+ */
+MyShows.prototype.listsRemoveEpisode = async function(id, list) {
+  try {
+    const response = await this.axios.post('', {
+      ...this.defaultParams,
+      method: 'lists.RemoveEpisode',
+      params: {
+        id,
+        list,
+      },
+    })
+
+    const { result } = response.data
+
+    if (result) {
+      return { id, list, result }
     } else {
       return { error: response.data.error }
     }
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const axios = require('axios')
 
 const AUTH_URL = 'https://myshows.me/oauth/token'
